e3c9b007e994f13a4dc90ba7166f2d7fc923caaa
derek <derek@hl521.me>
2022-03-08 11:26:18 -0700

    Makefile fix

--- src/headers/definitions.h
+++
@@ -1,3 +0,0 @@
-#define WORKDIR ../
-#define STYLES style\ sheets
-#define STYLESHEETS --css=$(WORKDIR)$(STYLES)/columns.css --css=$(WORKDIR)$(STYLES)/colors.css --css=$(WORKDIR)$(STYLES)/fonts.css --css=$(WORKDIR)$(STYLES)/photos.css --css=$(WORKDIR)$(STYLES)/navbar.css  

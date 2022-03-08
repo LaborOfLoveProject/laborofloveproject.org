1c6b2ce22c0d1ee2d5b312b44868d9298929beab
derek <derek@hl521.me>
2022-03-08 14:21:52 -0700

    Correct makefile

--- src/javascript/tailwind.config.js
+++ src/javascript/tailwind.config.js
@@ -1,7 +1,32 @@
 module.exports = {
 	content: ["./src/**/*.{html,js}"],
 	theme: {
-		extend: {},
+		screens: {
+			sm: '480px',
+			md: '768px',
+			lg: '976px',
+			xl: '1440px',
+		},
+		colors: {
+			'vinta-salmon': '#e15e5b',
+			'vinta-blue': {
+				ice: '#355370',
+				navy: '#2c2c42',
+			},
+		},
+		fontFamily: {
+			sans: ['Graphik', 'sans-serif'],
+			serif: ['Merriweather', 'serif'],
+		},
+		extend: {
+			spacing: {
+				'128': '32rem',
+				'144': '36rem',
+			},
+			borderRadius: {
+				'4xl': '2rem',
+			}
+		}
 	},
 	plugins: [],
 }
